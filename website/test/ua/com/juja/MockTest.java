998a5801949b84b43ea49a1e40f607255fa6a8bd
Viktor Kuchyn <kuchin.victor@gmail.com>
2015-06-24 23:49:57 +0700

    First commit with mock gradle build

---
+++ website/test/ua/com/juja/MockTest.java
@@ -0,0 +1,17 @@
+package ua.com.juja;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * User: viktor
+ * Date: 6/24/15
+ */
+public class MockTest {
+	@Test
+	public void deleteMe() throws Exception {
+		assertThat(true, is(true));
+	}
+}
