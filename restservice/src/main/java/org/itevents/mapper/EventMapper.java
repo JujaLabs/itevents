fe834d64daff9174070f0c5fd9304a9e9c2f8108
Cherepanov Roman <romach007@gmail.com>
2015-07-12 15:05:38 +0300

    #6 Fix mapping due table structure

--- restservice/src/main/java/org/itevents/mapper/EventMapper.java
+++ restservice/src/main/java/org/itevents/mapper/EventMapper.java
@@ -7,41 +7,50 @@ import org.itevents.model.Location;
 import java.util.List;
 
 public interface EventMapper {
-    @Select("SELECT id, name, date FROM event WHERE id = #{id}")
+    @Select("SELECT id, title, event_date, create_date, reg_link, address, contact FROM events WHERE id = #{id}")
     @Results(value = {
             @Result(property = "id", column = "id"),
+            @Result(property = "eventDate", column = "event_date"),
+            @Result(property = "createDate", column = "create_date"),
+            @Result(property = "regLink", column = "reg_link"),
             @Result(property = "location", column = "id", javaType = Location.class, one=@One(select="selectLocation"))
     })
     Event getEvent(long id);
 
-    @Select("SELECT id, name, date FROM event")
+    @Select("SELECT id, title, event_date, create_date, reg_link, address, contact FROM events")
     @Results(value = {
             @Result(property = "id", column = "id"),
+            @Result(property = "eventDate", column = "event_date"),
+            @Result(property = "createDate", column = "create_date"),
+            @Result(property = "regLink", column = "reg_link"),
             @Result(property = "location", column = "id", javaType = Location.class, one=@One(select="selectLocation"))
     })
     List<Event> getAllEvents();
 
-    @Select("SELECT id, name, date FROM event WHERE ST_DWithin(ST_MakePoint(point[0],point[1])::geography, ST_MakePoint(#{location.longitude},#{location.latitude})::geography, #{radius})")
+    @Select("SELECT id, title, event_date, create_date, reg_link, address, contact FROM events WHERE ST_DWithin(point)::geography, ST_MakePoint(#{location.longitude},#{location.latitude})::geography, #{radius})")
     @Results(value = {
             @Result(property = "id", column = "id"),
+            @Result(property = "eventDate", column = "event_date"),
+            @Result(property = "createDate", column = "create_date"),
+            @Result(property = "regLink", column = "reg_link"),
             @Result(property = "location", column = "id", javaType = Location.class, one=@One(select="selectLocation"))
     })
     List<Event> getFutureEventsInRadius(@Param("location")Location location, @Param("radius")long radius);
+
+    @Insert("INSERT INTO events(title, event_date, create_date, reg_link, address, point, contact) VALUES(#{name}, #{eventDate}, #{createDate}, #{regLink}, #{address}, point(#{location.longitude},#{location.latitude}), #{contact})")
+    void addEvent(Event event);
+
+    @Update("UPDATE events SET title=#{title}, event_date=#{eventDate}, create_date=#{createDate}, reg_link=#{regLink}, address=#{address}, point= point(#{location.longitude},#{location.latitude}), contact=#{contact} WHERE id =#{id}")
+    void updateEvent(Event event);
+
+    @Delete("DELETE FROM events WHERE id =#{id}")
+    void removeEvent(long id);
 
     @Results({
             @Result(property = "id", column = "id"),
             @Result(property = "longitude", column = "longitude"),
             @Result(property = "latitude", column = "latitude")
     })
-    @Select("SELECT point[0] AS longitude, point[1] AS latitude FROM event WHERE id = #{id}")
+    @Select("SELECT ST_X(point) AS longitude, ST_Y(point) AS latitude FROM events WHERE id = #{id}")
     Location selectLocation(long id);
-
-    @Insert("INSERT INTO event(name, date, point) VALUES(#{name}, #{date}, point(#{location.longitude},#{location.latitude}))")
-    void addEvent(Event event);
-
-    @Update("UPDATE event SET name=#{name}, date =#{date}, point= point(#{location.longitude},#{location.latitude}) WHERE id =#{id}")
-    void updateEvent(Event event);
-
-    @Delete("DELETE FROM event WHERE id =#{id}")
-    void removeEvent(long id);
 }
