e5cdfa9fb765741dd065c4a3b41a83b7c1da29c3
Alexander Vlasov <vaa25@yandex.ru>
2015-07-12 20:24:55 +0300

    add  data at users table

--- restservice/src/main/java/org/itevents/mapper/StatisticMapper.java
+++ restservice/src/main/java/org/itevents/mapper/StatisticMapper.java
@@ -14,7 +14,7 @@ public interface StatisticMapper {
     @Select("SELECT id, event_id, date, user_id FROM statistic WHERE event_id = #{event_id}")
     Statistic selectStatistic(@Param("event_id") int event_id);
 
-    @Insert("INSERT INTO statistic(event_id, user_id) VALUES(#{event_id}, CURDATE(),  user_id)")
+    @Insert("INSERT INTO statistic(event_id, date, user_id) VALUES(#{event_id}, NOW(),  #{user_id})")
     void addClick(@Param("event_id") int event_id, @Param("user_id") int user_id);
 
     @Delete("DELETE FROM statistic WHERE id =#{id}")
