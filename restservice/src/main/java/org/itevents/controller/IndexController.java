880111dd75ac1d4e8f9a40f67cb35b542a10157b
Vitaliy <GarazdVZ@gmail.com>
2015-07-16 23:11:27 +0300

    Update IndexController

--- restservice/src/main/java/org/itevents/controller/IndexController.java
+++ restservice/src/main/java/org/itevents/controller/IndexController.java
@@ -1,7 +1,7 @@
 package org.itevents.controller;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
