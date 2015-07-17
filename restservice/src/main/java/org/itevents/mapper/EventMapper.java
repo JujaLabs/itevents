d60df25348abdb069c3d17f758e16131b99bb6c2
standpoint <v.kamenskyi@gmail.com>
2015-07-17 11:36:08 +0300

    Issue_#7 Resource
    Misc. fix for the query syntax

--- restservice/src/main/java/org/itevents/mapper/EventMapper.java
+++ restservice/src/main/java/org/itevents/mapper/EventMapper.java
@@ -43,7 +43,7 @@ public interface EventMapper {
             @Result(property = "contact", column = "contact"),
             @Result(property = "location", column = "id", javaType = Location.class, one=@One(select="selectLocation"))
     })
-    @Select("SELECT id, title, event_date, create_date, reg_link, address, contact FROM events WHERE ST_DWithin(point)::geography, ST_MakePoint(#{location.longitude},#{location.latitude})::geography, #{radius})")
+    @Select("SELECT id, title, event_date, create_date, reg_link, address, contact FROM events WHERE ST_DWithin(point::geography, ST_MakePoint(#{location.longitude},#{location.latitude})::geography, #{radius})")
     List<Event> getFutureEventsInRadius(@Param("location")Location location, @Param("radius")int radius);
 
     @Insert("INSERT INTO events(title, event_date, create_date, reg_link, address, point, contact) VALUES(#{title}, #{eventDate}, #{createDate}, #{regLink}, #{address}, ST_MakePoint(#{location.longitude},#{location.latitude}), #{contact})")
