dbac64ca83ea00347544d5beca88b5d2df6b6097
Alexander Vlasov <vaa25@yandex.ru>
2015-07-22 11:01:37 +0300

    fix Role.toString()

--- restservice/src/main/java/org/itevents/model/Role.java
+++ restservice/src/main/java/org/itevents/model/Role.java
@@ -41,9 +41,9 @@ public class Role {
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Event ");
+        final StringBuilder sb = new StringBuilder("Role ");
         sb.append("id=").append(id);
-        sb.append(", name='").append(name);
+        sb.append(", name=").append(name);
         return sb.toString();
     }
 
