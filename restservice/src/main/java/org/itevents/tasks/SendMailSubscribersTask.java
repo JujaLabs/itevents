7176b01bac37b1285bdf11088cccb313c8265a15
Roman Ilnitsky <ramax@ex.ua>
2015-07-31 00:00:29 +0300

    add sendMailSubcribersTask

---
+++ restservice/src/main/java/org/itevents/tasks/SendMailSubscribersTask.java
@@ -0,0 +1,21 @@
+package org.itevents.tasks;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ramax on 7/30/15.
+ */
+
+public class SendMailSubscribersTask {
+
+    private static int i;
+
+    public void sendMails(){
+        System.out.println("Send mail" + i++);
+    }
+
+}
