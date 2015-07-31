0cecda89a76b33814a4e6ca30c80a80222a43cd3
Alexander Vlasov <vaa25@yandex.ru>
2015-07-31 23:03:02 +0300

    fix radius  in event filter, add dynamic tests

--- restservice/src/test/java/org/itevents/EventServiceTest.java
+++ restservice/src/test/java/org/itevents/EventServiceTest.java
@@ -36,12 +36,48 @@ public class EventServiceTest {
     }
 
     @Test
-    public void testGetFilteredEvents() {
+    public void testGetFilteredEventsKyivJava() {
         List<Event> expectedEvents = new ArrayList<>();
         FilterEventParams params = new FilterEventParams();
         params.setTechTags(new Integer[]{1});
         params.setCityId(1);
         expectedEvents.add(eventService.getEvent(1));
+        List<Event> returnedEvents = eventService.getFilteredEvents(params);
+        Assert.assertEquals(expectedEvents, returnedEvents);
+    }
+
+    @Test
+    public void testGetFilteredEventsBoyarkaPayed() {
+        List<Event> expectedEvents = new ArrayList<>();
+        FilterEventParams params = new FilterEventParams();
+        params.setCityId(3);
+        params.setPayed(true);
+        expectedEvents.add(eventService.getEvent(6));
+        List<Event> returnedEvents = eventService.getFilteredEvents(params);
+        Assert.assertEquals(expectedEvents, returnedEvents);
+    }
+
+    @Test
+    public void testGetFilteredEventsPhpAntSql() {
+        List<Event> expectedEvents = new ArrayList<>();
+        FilterEventParams params = new FilterEventParams();
+        params.setTechTags(new Integer[]{3, 7, 10});
+        expectedEvents.add(eventService.getEvent(4));
+        expectedEvents.add(eventService.getEvent(3));
+        expectedEvents.add(eventService.getEvent(7));
+        List<Event> returnedEvents = eventService.getFilteredEvents(params);
+        Assert.assertEquals(expectedEvents, returnedEvents);
+    }
+
+    @Test
+    public void testGetFilteredEventsInRadius() {
+        List<Event> expectedEvents = new ArrayList<>();
+        FilterEventParams params = new FilterEventParams();
+        params.setLatitude(50.454605);
+        params.setLongitude(30.403965);
+        params.setRadius(5000);
+        expectedEvents.add(eventService.getEvent(2));
+        expectedEvents.add(eventService.getEvent(3));
         List<Event> returnedEvents = eventService.getFilteredEvents(params);
         Assert.assertEquals(expectedEvents, returnedEvents);
     }
--- restservice/src/test/java/org/itevents/mapper/EventMapperTest.java
+++ restservice/src/test/java/org/itevents/mapper/EventMapperTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by vaa25 on 22.07.2015.
@@ -28,10 +29,17 @@ public class EventMapperTest {
     private CityMapper cityMapper;
 
     @Test
-    public void testGetEvent() throws Exception {
-        Event expected = new Event(1, "Java", new GregorianCalendar(2015, 6, 10).getTime(), null,
+    public void testGetEvent1() throws Exception {
+        Event expectedEvent = new Event(1, "Java", new GregorianCalendar(2015, 6, 10).getTime(), null,
                 "http://www.java.com.ua", "Beresteyska", new Location(50.458585, 30.742017), "java@gmail.com", 0,
                 currencyMapper.getCurrency(1), cityMapper.getCity(1));
-        assertEquals(expected, eventMapper.getEvent(1));
+        Event returnedEvent = eventMapper.getEvent(1);
+        assertEquals(expectedEvent, returnedEvent);
+    }
+
+    @Test
+    public void testGetEvent0() {
+        Event returnedEvent = eventMapper.getEvent(0);
+        assertNull(returnedEvent);
     }
 }
