9bc6bfb5ebf40df538be8d3debc3d8647104e6d3
alex-anakin <aleksandr.anakin@gmail.com>
2015-08-07 01:54:42 +0300

    spring dbunit tests updated

--- restservice/src/test/java/org/itevents/service/EventServiceDbTest.java
+++ restservice/src/test/java/org/itevents/service/EventServiceDbTest.java
@@ -2,6 +2,8 @@ package org.itevents.service;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import org.itevents.model.Event;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,10 +14,13 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration("../../../spring-test-context.xml")
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
         DbUnitTestExecutionListener.class })
 public class EventServiceDbTest {
@@ -24,12 +29,25 @@ public class EventServiceDbTest {
     private EventService eventService;
 
     @Test
-    @DatabaseSetup("events.xml")
-    public void testGetEventById() {
+    @DatabaseSetup("../../../dbunit/initialEvents.xml")
+    public void testGetAllEvents() {
         List<Event> events = eventService.getAllEvents();
         int expectedValue = 4;
         int actualValue = events.size();
         Assert.assertEquals(expectedValue, actualValue);
     }
 
+    @Test
+    @DatabaseSetup(value = "../../../dbunit/initialEvents.xml")
+    @ExpectedDatabase(value = "../../../dbunit/expectedEvents.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void testAddEvent() {
+        Event event = new Event();
+        event.setTitle("Front End");
+        Calendar calendar = new GregorianCalendar(2015,11,5,14,0,0);
+        event.setEventDate(calendar.getTime());
+        event.setRegLink("http://www.frontend.com.ua");
+        event.setAddress("Akademgorodok");
+        event.setContact("frontend@gmail.com");
+        eventService.addEvent(event);
+    }
 }
