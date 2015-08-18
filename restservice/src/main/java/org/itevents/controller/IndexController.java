8785a6964543f6bfdeeb258239ce82f35e44c8c7
Alexander Vlasov <vaa25@yandex.ru>
2015-08-18 15:09:24 +0300

    add ss xml config

--- restservice/src/main/java/org/itevents/controller/IndexController.java
+++ restservice/src/main/java/org/itevents/controller/IndexController.java
@@ -18,10 +18,10 @@ public class IndexController {
 	}
 
 	@RequestMapping(value = "login")
-	public String adminPage(ModelMap model) {
+	public String loginPage(ModelMap model) {
 		model.addAttribute("title", "Spring Security Hello World");
 		model.addAttribute("message", "This is protected page!");
-		return "admin";
+		return "login";
 	}
 
 	@RequestMapping(value = "logout", method = RequestMethod.GET)
@@ -30,5 +30,12 @@ public class IndexController {
 		model.addAttribute("message2", "Log in");
 		model.addAttribute("message3", "Log out");
 		return "index";
+	}
+
+	@RequestMapping(value = "admin")
+	public String adminPage(ModelMap model) {
+		model.addAttribute("title", "Spring Security Hello World");
+		model.addAttribute("message", "This is protected page!");
+		return "admin";
 	}
 }
