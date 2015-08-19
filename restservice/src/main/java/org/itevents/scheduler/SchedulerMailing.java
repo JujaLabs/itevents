821cb1e5be412098b9d4f1664426a4fe936cbac2
Roman Ilnitsky <ramax@ex.ua>
2015-08-19 17:27:38 +0300

    change bug

--- restservice/src/main/java/org/itevents/scheduler/SchedulerMailing.java
+++ restservice/src/main/java/org/itevents/scheduler/SchedulerMailing.java
@@ -1,19 +1,6 @@
 package org.itevents.scheduler;
 
-import org.itevents.model.Event;
-import org.itevents.model.User;
-import org.itevents.service.SendGridService;
-import org.itevents.service.UserEventService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.config.Task;
-import org.springframework.scheduling.support.CronSequenceGenerator;
-import org.springframework.scheduling.support.CronTrigger;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ramax on 7/30/15.
