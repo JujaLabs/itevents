79e03039b266ae373659052406dc378f898b3edd
Anton Erofeev <anttoshka@gmail.comgit config --global user.email anttoshka@gmail.com>
2015-08-27 14:19:39 +0300

    fix hashCode in Event

--- restservice/src/main/java/org/itevents/model/Event.java
+++ restservice/src/main/java/org/itevents/model/Event.java
@@ -206,7 +206,7 @@ public class Event implements Serializable {
         result = 31 * result + (address != null ? address.hashCode() : 0);
         result = 31 * result + (location != null ? location.hashCode() : 0);
         result = 31 * result + (contact != null ? contact.hashCode() : 0);
-        result = 31 * result + (free ? 1 : 0);
+        result = 31 * result + (free != null ? 1 : 0);
         result = 31 * result + (price != null ? price.hashCode() : 0);
         result = 31 * result + (currency != null ? currency.hashCode() : 0);
         result = 31 * result + (city != null ? city.hashCode() : 0);
