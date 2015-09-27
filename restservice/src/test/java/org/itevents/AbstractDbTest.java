88bdf6ebe0146d32c670ebd7db3b4fffe5d62878
Alexander Vlasov <vaa25@yandex.ru>
2015-09-27 22:08:24 +0300

    extract dbunit test data path to constant

--- restservice/src/test/java/org/itevents/AbstractDbTest.java
+++ restservice/src/test/java/org/itevents/AbstractDbTest.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
-@TestPropertySource("classpath:test.local.properties")
+@TestPropertySource("classpath:test-local.properties")
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
         TransactionDbUnitTestExecutionListener.class})
 @Transactional
