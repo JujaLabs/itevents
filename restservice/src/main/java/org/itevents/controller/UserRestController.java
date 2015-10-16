f8c3b3be0b1e9a5a5929f7bdbeabdedd0f8d96ae
Alexander Vlasov <vaa25@yandex.ru>
2015-10-16 12:24:20 +0300

    fix add/remove user in user controller

--- restservice/src/main/java/org/itevents/controller/UserRestController.java
+++ restservice/src/main/java/org/itevents/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package org.itevents.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.itevents.model.NewUser;
 import org.itevents.model.User;
@@ -11,12 +12,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
 /**
  * Created by vaa25 on 16.10.2015.
  */
+@RestController
+@Api("Users")
 @RequestMapping("/users")
 public class UserRestController {
     @Inject
@@ -27,6 +31,9 @@ public class UserRestController {
     @RequestMapping(method = RequestMethod.POST, value = "/register")
     @ApiOperation(value = "Registers new Subscriber ")
     public ResponseEntity registerNewSubscriber(@ModelAttribute NewUser newUser) {
+        if (exists(newUser)) {
+            return new ResponseEntity(HttpStatus.IM_USED);
+        }
         final int SUBSCRIBER_ROLE_ID = -3;
         User user = new User();
         user.setLogin(newUser.getLogin());
@@ -34,16 +41,18 @@ public class UserRestController {
         user.setRole(roleService.getRole(SUBSCRIBER_ROLE_ID));
         userService.addUser(user);
         return new ResponseEntity(HttpStatus.OK);
-        //todo if user exists
+    }
+
+    private boolean exists(NewUser newUser) {
+        return userService.getUserByName(newUser.getLogin()) != null;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
     @ApiOperation(value = "Removes user from database ")
-    public ResponseEntity getFilteredEvents() {
+    public ResponseEntity removeUser() {
         User user = getUserFromSecurityContext();
         userService.removeUser(user);
         return new ResponseEntity(HttpStatus.OK);
-        //todo if user absent
     }
 
     private User getUserFromSecurityContext() {
