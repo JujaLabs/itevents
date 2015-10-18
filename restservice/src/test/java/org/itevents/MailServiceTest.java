b0f6190455cf93bab214b1034681c97de6ce8f45
Alex Anakin <alex.anakin@qoderoom.com>
2015-10-18 12:41:46 +0300

    REVIEW : made comments

--- restservice/src/test/java/org/itevents/MailServiceTest.java
+++ restservice/src/test/java/org/itevents/MailServiceTest.java
@@ -15,12 +15,16 @@ import javax.inject.Inject;
 @ContextConfiguration(locations = { "classpath*:applicationContext.xml" })
 public class MailServiceTest {
 
+    //@alex-anakin:  create real particular e-mail address for testing needs
     String userMail ="example@it-events.com"; //Enter your e-mail, which will receive letter
     String htmlLetter ="<html><head><title>TEST MAIL</title></head><body><h1>It's test mail to you "+userMail+"</h1><br><img src=\"http://phd.chnebu.ch/images/Java.png\"></body></html>";
 
+    //@alex-anakin: fix mistake in variable name
     @Inject
     MailService mailsService;
 
+    //@alex-anakin: make tests for methods #1 and #2 from service
+    // tests should have assertion of results
     @Test
     public void testSendMail(){
         mailsService.sendMail(htmlLetter,userMail);
