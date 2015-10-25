303aeb45ea538d2342f0f7d5ca5bccc0317a94d3
Alexander Vlasov <vaa25@yandex.ru>
2015-10-25 10:42:06 +0200

    fix db data

--- restservice/src/main/resources/liquibase/data/029-insert_data_to_filter.sql
+++ restservice/src/main/resources/liquibase/data/029-insert_data_to_filter.sql
@@ -1,10 +1,11 @@
 --liquibase formatted sql
 
 --changeset vaa25:29
-INSERT INTO filter (id, "limit", city_id, free, longitude, latitude, radius) VALUES
-  (-1, NULL, -1, NULL, NULL, NULL, NULL),
-  (-2, NULL, NULL, TRUE, NULL, NULL, NULL),
-  (-3, NULL, -1, FALSE, NULL, NULL, NULL),
-  (-4, NULL, NULL, NULL, NULL, NULL, NULL)
+INSERT INTO filter (id, "limit", create_date, city_id, free, longitude, latitude, radius) VALUES
+  (-1, NULL, '10.07.2015', -1, NULL, NULL, NULL, NULL),
+  (-2, NULL, '11.07.2015', NULL, TRUE, NULL, NULL, NULL),
+  (-3, NULL, '12.07.2015', -1, FALSE, NULL, NULL, NULL),
+  (-4, NULL, '13.07.2015', NULL, NULL, NULL, NULL, NULL),
+  (-5, NULL, '14.07.2015', -1, FALSE, NULL, NULL, NULL)
 
 --rollback DELETE FROM filter;
--- restservice/src/main/resources/liquibase/data/033-insert_to_user_filter.sql
+++ restservice/src/main/resources/liquibase/data/033-insert_to_user_filter.sql
@@ -3,6 +3,7 @@
 --changeset vaa25:33
 INSERT INTO user_filter (user_id, filter_id) VALUES
   (-1, -1),
+  (-1, -5),
   (-4, -3),
   (-2, -2),
   (-3, -4)
