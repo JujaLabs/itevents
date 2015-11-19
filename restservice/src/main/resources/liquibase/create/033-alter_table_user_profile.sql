cb448f60455c14d0caf09b52d03cc37bc805e9eb
Igor Maksymov <ivmaksymov@gmail.com>
2015-11-19 21:56:10 +0800

    fix refactoring

--- restservice/src/main/resources/liquibase/create/033-alter_table_user_profile.sql
+++ restservice/src/main/resources/liquibase/create/033-alter_table_user_profile.sql
@@ -1,11 +1,12 @@
--- --liquibase formatted sql
---changeset Igor:33
+-- liquibase formatted sql
+-- changeset Igor:33
 
 ALTER TABLE user_profile
-DROP CONSTRAINT user_profile_role_id_fkey,
-ADD CONSTRAINT user_profile_role_id_fkey
-FOREIGN KEY (role_id) REFERENCES role ON DELETE CASCADE;
+    DROP CONSTRAINT user_profile_role_id_fkey,
+    ADD CONSTRAINT user_profile_role_id_fkey
+    FOREIGN KEY (role_id) REFERENCES role ON DELETE CASCADE;
 
--- rollback DROP CONSTRAINT user_profile_role_id_fkey
--- ADD CONSTRAINT user_profile_role_id_fkey
--- FOREIGN KEY (role_id) REFERENCES role;
+-- rollback ALTER TABLE user_profile
+--    DROP CONSTRAINT user_profile_role_id_fkey
+--    ADD CONSTRAINT user_profile_role_id_fkey
+--    FOREIGN KEY (role_id) REFERENCES role;
