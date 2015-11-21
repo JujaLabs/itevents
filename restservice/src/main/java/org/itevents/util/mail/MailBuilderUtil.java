74119f051ed6a34b61615761f3a8957106a6f58f
Garazd <GarazdVZ@gmail.com>
2015-11-21 22:10:06 +0200

    Added url in MailBuilderUtil

--- restservice/src/main/java/org/itevents/util/mail/MailBuilderUtil.java
+++ restservice/src/main/java/org/itevents/util/mail/MailBuilderUtil.java
@@ -6,6 +6,7 @@ import org.itevents.model.User;
 import org.itevents.util.OneTimePassword.OtpGen;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Component;
 
 import javax.xml.bind.JAXBContext;
@@ -38,9 +39,9 @@ public class MailBuilderUtil {
         return buildMailFromXmlEvents(buildXmlFromEventList(events));
     }
     // ÁÐÀÍ× 48
-    public String buildHtmlFromUserOtp(User user, OtpGen otpGen)  throws ParseException, JAXBException, IOException,
+    public String buildHtmlFromUserOtp(User user, OtpGen otpGen, HttpRequest url)  throws ParseException, JAXBException, IOException,
             TransformerException {
-        return buildMailFromXmlUserOtp(BuildXmlFromUserOtp(user, otpGen));
+        return buildMailFromXmlUserOtp(BuildXmlFromUserOtp(user, otpGen, url));
     }
 
     private String buildXmlFromEventList(List<Event> events) throws JAXBException {
@@ -73,12 +74,11 @@ public class MailBuilderUtil {
     }
 
     //    ÁÐÀÍ× 48
-    private String BuildXmlFromUserOtp(User user, OtpGen otpGen) throws JAXBException {
+    private String BuildXmlFromUserOtp(User user, OtpGen otpGen, HttpRequest url) throws JAXBException {
         UserOtpXmlWrapper userOtpXmlWrapper = new UserOtpXmlWrapper();
         userOtpXmlWrapper.setUser(user);
         userOtpXmlWrapper.setOtpGen(otpGen);
-        userOtpXmlWrapper.getUrl();
-
+        userOtpXmlWrapper.getUrl(url);
         Marshaller marshaller = JAXBContext.newInstance(UserOtpXmlWrapper.class).createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
@@ -125,7 +125,7 @@ public class MailBuilderUtil {
     @XmlAccessorType(XmlAccessType.FIELD)
     private static class UserOtpXmlWrapper {
         @XmlElement(name = "url")
-        private static String url;
+        private static HttpRequest url;
         @XmlElement(name = "user")
         private User user;
         @XmlElement(name = "otp")
@@ -150,11 +150,11 @@ public class MailBuilderUtil {
             this.otpGen = otpGen;
         }
 
-        public  String getUrl() {
+        public HttpRequest getUrl(HttpRequest url) {
             return url;
         }
 
-        public void setUrl(String url) {
+        public void setUrl(HttpRequest url) {
             this.url = url;
         }
     }
