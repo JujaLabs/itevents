5b0c2d81e5c552c8ac9abd5cec9be05880f4a20e
Igor Maksymov <ivmaksymov@gmail.com>
2015-12-08 09:01:11 +0200

    user_event table columns renamed, added column created_date, tests refactored

--- restservice/src/main/java/org/itevents/dao/mybatis/mapper/UserMapper.java
+++ restservice/src/main/java/org/itevents/dao/mybatis/mapper/UserMapper.java
@@ -41,6 +41,6 @@ public interface UserMapper extends UserDao {
     @Override
     @ResultMap("getUser-int")
     @Select("SELECT * FROM user_profile up JOIN user_event ue ON up.id=ue.user_id " +
-            "WHERE ue.event_id = #{event.id} AND unassign_date IS NULL")
+            "WHERE ue.event_id = #{event.id} AND deleted_date IS NULL")
     List<User> getUsersByEvent(@Param("event") Event event);
 }
--- restservice/src/main/resources/liquibase/create/034-alter_table_user_event.sql
+++ restservice/src/main/resources/liquibase/create/034-alter_table_user_event.sql
@@ -3,14 +3,16 @@
 
 ALTER TABLE user_event
   DROP CONSTRAINT user_event_pkey,
-  ADD COLUMN unassign_date TIMESTAMP DEFAULT NULL,
-  ADD COLUMN unassign_reason VARCHAR(250)DEFAULT NULL,
+  ADD COLUMN created_date TIMESTAMP DEFAULT now(),
+  ADD COLUMN deleted_date TIMESTAMP DEFAULT NULL ,
+  ADD COLUMN deleted_reason VARCHAR(250)DEFAULT NULL,
   ADD id SERIAL PRIMARY KEY NOT NULL;
 
 -- rollback ALTER TABLE user_event
 -- DROP CONSTRAINT user_event_pkey,
--- DROP COLUMN unassign_date,
--- DROP COLUMN  unassign_reason,
+-- DROP COLUMN created_date,
+-- DROP COLUMN deleted_date,
+-- DROP COLUMN deleted_reason,
 -- DROP COLUMN id,
 -- ADD PRIMARY KEY (user_id, event_id),
 -- UNIQUE (user_id, event_id);
