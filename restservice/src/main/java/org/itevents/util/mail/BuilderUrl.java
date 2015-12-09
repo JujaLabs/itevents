a5915a7f16942779973d253b4e8bb7cd2f169823
Garazd <GarazdVZ@gmail.com>
2015-12-09 21:02:16 +0200

    Changing the properties on the annotation

--- restservice/src/main/java/org/itevents/util/mail/BuilderUrl.java
+++ restservice/src/main/java/org/itevents/util/mail/BuilderUrl.java
@@ -1,34 +1,38 @@
 package org.itevents.util.mail;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import java.io.IOException;
-import java.util.Properties;
 
-@Component
+@PropertySource("local.property")
 public class BuilderUrl {
 
-    private String url;
+    @Value("${serverName}")
+    private String serverName;
+    @Value("${httpPort}")
+    private String httpPort;
 
     public BuilderUrl() {
     }
 
-    public String getUrl() {
-        return url;
+    public String getServerName() {
+        return serverName;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getHttpPort() {
+        return httpPort;
+    }
+
+    public void setHttpPort(String httpPort) {
+        this.httpPort = httpPort;
     }
 
     public void buildUrl() throws IOException {
-        Properties prop = new Properties();
-
-        prop.load(getClass().getClassLoader().getResourceAsStream("local.properties"));
-
-        String serverName = prop.getProperty("serverName");
-        String httpPort = prop.getProperty("httpPort");
-
-        url = String.valueOf(new StringBuilder("http://" + serverName + ":" + httpPort));
+        String url = String.valueOf(new StringBuilder("http://" + serverName + ":" + httpPort));
     }
 }
