e7578711f1b70c23d94a7e33bd3dba0b582add71
Zavodnyuk <lovemaxonly@gmail.com>
2015-12-22 14:06:53 +0200

    refactored tests

--- restservice/src/test/java/org/itevents/service/transactional/MailReminderAboutEventServiceTest.java
+++ restservice/src/test/java/org/itevents/service/transactional/MailReminderAboutEventServiceTest.java
@@ -1,13 +1,18 @@
 package org.itevents.service.transactional;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
+import org.itevents.dao.EventDao;
+import org.itevents.dao.UserDao;
 import org.itevents.model.Event;
 import org.itevents.model.User;
+import org.itevents.service.mail.MailMock;
 import org.itevents.service.mail.MailReminderAboutEventService;
 import org.itevents.test_utils.BuilderUtil;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,7 +22,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by ramax on 11/7/15.
@@ -26,34 +32,41 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(locations = "classpath*:applicationContext.xml")
 public class MailReminderAboutEventServiceTest {
 
+    @InjectMocks
     @Inject
     private MailReminderAboutEventService mailReminderAboutEventService;
 
-    private static final int MILLISECONDS_TO_DAYS = 24 * 60 * 60 * 1000;
+    @Mock
+    EventDao eventDao;
 
+    @Mock
+    UserDao userDao;
 
-    private int shouldReturnDaysTillEvent() throws ParseException {
-        long result;
-        // Get difference of days between today and date of event with id -1
-        result = (BuilderUtil.buildEventJava().getEventDate().getTime()) //day of event with id -1
-                - (new Date().getTime()); //today
-        return (int) (result / MILLISECONDS_TO_DAYS) + 1;
+    @Mock
+    MailMock mailMock;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void shouldReturnEventByDaysTillEventTest() throws ParseException {
-        List<Event> expectedEvents = new ArrayList<Event>();
-        expectedEvents.add(BuilderUtil.buildEventJava());
-        List <Event> returnedEvents = mailReminderAboutEventService.getEventsByDaysTillEvent(shouldReturnDaysTillEvent());
-        assertEquals(expectedEvents,returnedEvents);
-    }
+    public void executeTest() throws ParseException {
 
-    @Test
-    public void shouldReturnUsersAndEventsByDaysTillEventTest() throws ParseException {
-        Multimap<User, Event> expectedUsersAndEvents = HashMultimap.create();
-        Multimap<User, Event> returnedUsersAndEvents = mailReminderAboutEventService.getUsersAndEventsByDaysTillEvent(shouldReturnDaysTillEvent());
-        expectedUsersAndEvents.put(BuilderUtil.buildUserGuest(), BuilderUtil.buildEventJava());
-        expectedUsersAndEvents.put(BuilderUtil.buildUserAnakin(), BuilderUtil.buildEventJava());
-        assertEquals(expectedUsersAndEvents,returnedUsersAndEvents);
+        List<Event> events = new ArrayList<Event>();
+        events.add(BuilderUtil.buildEventJava());
+        when(eventDao.getEventsByDate(any(Date.class))).thenReturn(events);
+
+        List<User> user = new ArrayList<User>();
+        user.add(BuilderUtil.buildSubscriberTest());
+        when(userDao.getUsersByEvent(any(Event.class))).thenReturn(user);
+
+        doNothing().when(mailMock).sendEmail(anyString(), anyString());
+
+        mailReminderAboutEventService.execute();
+
+        verify(eventDao).getEventsByDate(any(Date.class));
+        verify(userDao, times(events.size())).getUsersByEvent(any((Event.class)));
+        verify(mailMock, times(user.size())).sendEmail(anyString(), anyString());
     }
 }
