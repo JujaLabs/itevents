db0a25bce4063c16a4049c033b7853cc3eb0facc
Garazd <GarazdVZ@gmail.com>
2016-01-28 21:47:23 +0200

    Merge branches issue48 and master #48

--- restservice/src/main/java/org/itevents/dao/mybatis/mapper/UserMapper.java
+++ restservice/src/main/java/org/itevents/dao/mybatis/mapper/UserMapper.java
@@ -40,9 +40,14 @@ public interface UserMapper extends UserDao {
     void updateUser(User user);
 
     @Override
-    @ResultMap("org.itevents.dao.mybatis.mapper.EventMapper.getEvent-int")
-    @Select("SELECT * FROM event e JOIN user_event ue ON e.id=ue.event_id WHERE ue.user_id = #{user.id}")
-    List<Event> getUserEvents(@Param("user") User user);
+    @ResultMap("getUser-int")
+    @Select("SELECT * FROM user_profile up JOIN user_event ue ON up.id=ue.user_id " +
+            "WHERE ue.event_id = #{event.id} AND deleted_date IS NULL")
+    List<User> getUsersByEvent(@Param("event") Event event);
+
+    @ResultMap("getUser-int")
+    @Select("SELECT * FROM user_profile WHERE subscribed = TRUE")
+    List<User> getSubscribedUsers();
 
     @Insert("UPDATE user_profile SET isActive = true WHERE id = #{id}")
     void activateUser(User user);
@@ -64,12 +69,4 @@ public interface UserMapper extends UserDao {
 
     @Delete("DELETE FROM user_otp WHERE user_id = #{user.id}")
     void DeleteOtp(@Param("user")User user);
-    @ResultMap("getUser-int")
-    @Select("SELECT * FROM user_profile up JOIN user_event ue ON up.id=ue.user_id " +
-            "WHERE ue.event_id = #{event.id} AND deleted_date IS NULL")
-    List<User> getUsersByEvent(@Param("event") Event event);
-
-    @ResultMap("getUser-int")
-    @Select("SELECT * FROM user_profile WHERE subscribed = TRUE")
-    List<User> getSubscribedUsers();
 }
