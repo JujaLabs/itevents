7d592d70e3dc5d05da24ae8a11e4586cae7baba1
Cherepanov Roman <romach007@gmail.com>
2016-02-07 19:27:17 +0200

    #124: move security package to services

--- restservice/src/main/java/org/itevents/service/security/MySavedRequestAwareAuthenticationSuccessHandler.java
+++ restservice/src/main/java/org/itevents/service/security/MySavedRequestAwareAuthenticationSuccessHandler.java
@@ -1,4 +1,4 @@
-package org.itevents.controller.security;
+package org.itevents.service.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
--- restservice/src/main/java/org/itevents/service/security/RestAuthenticationEntryPoint.java
+++ restservice/src/main/java/org/itevents/service/security/RestAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package org.itevents.controller.security;
+package org.itevents.service.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
