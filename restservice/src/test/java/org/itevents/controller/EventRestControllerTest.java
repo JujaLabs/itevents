447d4f66134b065826dfd0da58488f37821a22ed
Cherepanov Roman <romach007@gmail.com>
2016-03-01 10:57:16 +0200

    #125: delete unused import, resolve merge conflict

--- restservice/src/test/java/org/itevents/controller/EventRestControllerTest.java
+++ restservice/src/test/java/org/itevents/controller/EventRestControllerTest.java
@@ -19,11 +19,9 @@ import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
