5586bb2c1e16fba2fb37f578b3ca5edd1fb1639c
Garazd <GarazdVZ@gmail.com>
2016-03-06 19:16:27 +0200

    #48: Refactoring test shouldReturnMailWithActivationLink

--- restservice/src/test/java/org/itevents/util/mail/MailBuilderUtilTest.java
+++ restservice/src/test/java/org/itevents/util/mail/MailBuilderUtilTest.java
@@ -1,22 +1,21 @@
 package org.itevents.util.mail;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.TransformerException;
+import org.apache.xalan.extensions.XSLProcessorContext;
+import org.apache.xalan.templates.ElemExtensionCall;
 import org.itevents.dao.model.Event;
 import org.itevents.dao.model.User;
 import org.itevents.test_utils.BuilderUtil;
 import org.itevents.util.OneTimePassword.OneTimePassword;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.inject.Inject;
-import javax.xml.bind.JAXBException;
-import javax.xml.transform.TransformerException;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -40,14 +39,10 @@ public class MailBuilderUtilTest {
         assertEquals(expectedDigestEmail, returnedDigestEmail);
     }
 
-    /*
-    * @TODO: FIX THIS TEST
-    * issue 156
-    * https://github.com/JuniorsJava/itevents/issues/156
-    */
     @Test
-    @Ignore
     public void shouldReturnMailWithActivationLink()  throws Exception {
+        XSLProcessorContext context = null;
+        ElemExtensionCall elem = null;
         int twoMonthInHours = 1440;
         User user = BuilderUtil.buildUserAnakin();
         oneTimePassword.generateOtp(twoMonthInHours);
