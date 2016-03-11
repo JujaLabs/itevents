174808e23055ec0521f048a307fdb449a251fe1b
Alexander Vlasov <vaa25@yandex.ru>
2016-03-11 10:44:43 +0200

    #169 add helloworld

--- crawler/src/main/java/org/itevents/service/CrawlerService.java
+++ crawler/src/main/java/org/itevents/service/CrawlerService.java
@@ -4,4 +4,7 @@ package org.itevents.service;
  * Created by vaa25 on 10.03.2016.
  */
 public class CrawlerService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
