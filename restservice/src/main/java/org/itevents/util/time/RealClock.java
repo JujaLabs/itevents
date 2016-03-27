54eb9ef480accb896e18378a2c9c4188fc28dcf0
Cherepanov Roman <romach007@gmail.com>
2016-03-27 16:25:27 +0300

    #172: CustomDateTime is used in RealClock and FrozenClock

--- restservice/src/main/java/org/itevents/util/time/RealClock.java
+++ restservice/src/main/java/org/itevents/util/time/RealClock.java
@@ -3,21 +3,18 @@ package org.itevents.util.time;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Date;
 
 @Component
 public class RealClock implements Clock {
     @Override
     public Date getNowDateTime() {
-        return new Date();
+        return new CustomDateTime().getDate();
     }
 
     // we use this method to get instance of LocalDateTime class that has convenient methods for date/time manipulation
     @Override
     public LocalDateTime getNowLocalDateTime() {
-        return LocalDateTime.ofInstant(
-                this.getNowDateTime().toInstant(), ZoneId.systemDefault()
-        );
+        return new CustomDateTime().getLocalDateTime();
     }
 }
--- restservice/src/test/java/org/itevents/test_utils/time/FrozenClock.java
+++ restservice/src/test/java/org/itevents/test_utils/time/FrozenClock.java
@@ -1,12 +1,12 @@
 package org.itevents.test_utils.time;
 
 import org.itevents.util.time.Clock;
+import org.itevents.util.time.CustomDateTime;
 import org.itevents.util.time.RealClock;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Date;
 
 /**
@@ -16,21 +16,19 @@ import java.util.Date;
 @Primary
 @Component
 public class FrozenClock implements Clock {
-    private final Date frozenDateTime;
+    private final CustomDateTime frozenDateTime;
 
     public FrozenClock() {
-        this.frozenDateTime = new Date();
+        this.frozenDateTime = new CustomDateTime();
     }
 
     @Override
     public Date getNowDateTime() {
-        return this.frozenDateTime;
+        return this.frozenDateTime.getDate();
     }
 
     @Override
     public LocalDateTime getNowLocalDateTime() {
-        return LocalDateTime.ofInstant(
-                this.getNowDateTime().toInstant(), ZoneId.systemDefault()
-        );
+        return this.frozenDateTime.getLocalDateTime();
     }
 }
