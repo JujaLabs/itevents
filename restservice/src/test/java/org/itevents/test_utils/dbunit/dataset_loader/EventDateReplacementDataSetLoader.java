9c6f05c565ee263828074c53d4e2673452a1d3fe
Cherepanov Roman <romach007@gmail.com>
2016-04-13 00:04:00 +0300

    #125: refactor EventDateReplacementDataSetLoader

--- restservice/src/test/java/org/itevents/test_utils/dbunit/dataset_loader/EventDateReplacementDataSetLoader.java
+++ restservice/src/test/java/org/itevents/test_utils/dbunit/dataset_loader/EventDateReplacementDataSetLoader.java
@@ -1,7 +1,11 @@
 package org.itevents.test_utils.dbunit.dataset_loader;
 
+import com.github.springtestdbunit.dataset.DataSetLoader;
+import com.github.springtestdbunit.dataset.FlatXmlDataSetLoader;
 import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.ReplacementDataSet;
 import org.itevents.util.time.DateTimeUtil;
 
 import java.util.ArrayList;
@@ -9,10 +13,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by roma on 13.10.15.
- */
-public class EventDateReplacementDataSetLoader extends AbstractReplacementDataSetLoader {
+public class EventDateReplacementDataSetLoader implements DataSetLoader {
 
     private static final String EVENT_TABLE_NAME = "event";
     private static final String EVENT_DATE_COLUMN_NAME = "event_date";
@@ -20,22 +21,28 @@ public class EventDateReplacementDataSetLoader extends AbstractReplacementDataSe
     private static final String DATE_TIME_FORMAT_FOR_DATABASE = "yyyy-MM-dd HH:mm:ss.SSS";
 
     @Override
-    protected void replace() throws DataSetException {
-        List<String> dateTemplates = getDateTemplatesFromDataSet();
-        for (String dateTemplate : dateTemplates) {
-            // if now is 13.10.15 17:23:45.678 then
-            // {now+5} > 18.10.15 17:23:45.678
-            replace(dateTemplate, buildFormattedDateFromDateTemplate(dateTemplate));
-        }
+    public IDataSet loadDataSet(Class<?> testClass, String location) throws Exception {
+        IDataSet dataSetFromXml = new FlatXmlDataSetLoader().loadDataSet(testClass, location);
+        List<String> dateTemplates = getDateTemplatesFromDataSet(dataSetFromXml);
+        return buildReplacementDataSetFromDateTemplates(dataSetFromXml, dateTemplates);
     }
 
-    private List<String> getDateTemplatesFromDataSet() throws DataSetException {
+    private List<String> getDateTemplatesFromDataSet(IDataSet dataSet) throws DataSetException {
         List<String> dateTemplates = new ArrayList<>();
-        ITable eventTable = this.dataSet.getTable(EVENT_TABLE_NAME);
+        ITable eventTable = dataSet.getTable(EVENT_TABLE_NAME);
         for (int rowIndex = 0; rowIndex < eventTable.getRowCount(); rowIndex++) {
             dateTemplates.add(eventTable.getValue(rowIndex, EVENT_DATE_COLUMN_NAME).toString());
         }
         return dateTemplates;
+    }
+
+    private ReplacementDataSet buildReplacementDataSetFromDateTemplates(IDataSet dataSet, List<String> dateTemplates) {
+        ReplacementDataSet replacementDataSet = new ReplacementDataSet(dataSet);
+        for (String dateTemplate : dateTemplates) {
+            String formattedDateFromDateTemplate =  buildFormattedDateFromDateTemplate(dateTemplate);
+            replacementDataSet.addReplacementSubstring(dateTemplate, formattedDateFromDateTemplate);
+        }
+        return replacementDataSet;
     }
 
     private String buildFormattedDateFromDateTemplate(String dateTemplate) {
